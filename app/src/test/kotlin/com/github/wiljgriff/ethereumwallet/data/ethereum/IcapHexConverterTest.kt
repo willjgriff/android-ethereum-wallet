a98459e7bfad2ee0cf495ae8a13ec92690914907
willjgriff <willjgriff@gmail.com>
2017-03-06 08:38:30 +0000

    Working on the IcapHexConverter.

--- app/src/test/kotlin/com/github/wiljgriff/ethereumwallet/data/ethereum/IcapHexConverterTest.kt
+++ app/src/test/kotlin/com/github/wiljgriff/ethereumwallet/data/ethereum/IcapHexConverterTest.kt
@@ -1,5 +1,6 @@
 package com.github.wiljgriff.ethereumwallet.data.ethereum
 
+import org.amshove.kluent.shouldBe
 import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
@@ -39,5 +40,35 @@ class IcapHexConverterTest {
         val actualNumber = subject.base36ToBase16("38O073KYGTWWZN0F2WZ0R8PX5ZPPZS")
         expectedNumber shouldEqual actualNumber
     }
+
+    @Test
+    fun createChecksum_returnsCorrectChecksum() {
+        val expectedChecksum = 8;
+        val actualChecksum = subject.createChecksum("P2J0C65CFU410SM2IXXO687WQO2HMJV")
+        expectedChecksum shouldEqual actualChecksum
+    }
+
+    @Test
+    fun verifyChecksum_withCorrectInputReturnsTrue() {
+        val addressIsValid = subject.verifyChecksum("P2J0C65CFU410SM2IXXO687WQO2HMJV", 8)
+        addressIsValid shouldBe true
+    }
+
+    @Test
+    fun verifyChecksum_withIncorrectInputReturnsFalse() {
+        val addressIsValid = subject.verifyChecksum("P2J0C65CFU410SM2IXXO687WQO2HMJW", 8)
+        addressIsValid shouldBe false
+    }
+
+    @Test
+    fun formatAsIban_createsCorrectString() {
+        
+    }
+
+    // 0xD69F2FF2893C73B5eF4959a2ce85Ab1B1d35CE6B should convert to
+    // iban:XE08P2J0C65CFU410SM2IXXO687WQO2HMJV
+
+    // XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS should convert to
+    // 00c5496aee77c1ba1f0854206a26dda82a81d6d8
 
 }
