89af9bde1d06841edfbce4ae8737b9cf4114a5e9
willjgriff <willjgriff@gmail.com>
2017-03-21 08:34:59 +0000

    Started enabling Dagger for Kotlin. Still working on it. Won't build.

--- app/src/main/kotlin/com/github/wiljgriff/ethereumwallet/ui/nodestatus/NodeStatusController.kt
+++ app/src/main/kotlin/com/github/wiljgriff/ethereumwallet/ui/nodestatus/NodeStatusController.kt
@@ -4,42 +4,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.github.wiljgriff.ethereumwallet.ethereum.node.Ethereum
 import com.github.wiljgriff.ethereumwallet.ui.nodestatus.mvp.NodeStatusPresenter
 import com.github.wiljgriff.ethereumwallet.ui.nodestatus.mvp.NodeStatusView
 import com.github.willjgriff.ethereumwallet.R
 import com.github.willjgriff.ethereumwallet.mvp.BaseMvpController
 import org.ethereum.geth.Header
 import org.ethereum.geth.PeerInfos
+import javax.inject.Inject
 
 /**
  * Created by Will on 20/03/2017.
  */
 class NodeStatusController : BaseMvpController<NodeStatusView, NodeStatusPresenter>(), NodeStatusView {
 
-    internal var mPeers: TextView? = null
-    internal var mHeaders: TextView? = null
+    private var peers: TextView? = null
+    private var headers: TextView? = null
 
-    override fun createPresenter(): NodeStatusPresenter {
-        return NodeStatusPresenter(Ethereum(applicationContext?.filesDir.toString() + "Hola"))
+    @Inject
+    lateinit var presenter: NodeStatusPresenter
+
+    constructor() : super() {
+        
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
-        val view = inflater.inflate(R.layout.controller_transactions, container, false)
-        mPeers = view.findViewById(R.id.controller_transactions_peers) as TextView?
-        mHeaders = view.findViewById(R.id.controller_transactions_text) as TextView?
-        return view
+
+    override fun createPresenter(): NodeStatusPresenter {
+        return presenter
     }
 
     override fun getMvpView(): NodeStatusView {
         return this
     }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
+        val view = inflater.inflate(R.layout.controller_transactions, container, false)
+        peers = view.findViewById(R.id.controller_transactions_peers) as TextView?
+        headers = view.findViewById(R.id.controller_transactions_text) as TextView?
+        return view
+    }
+
     override fun newHeader(header: Header) {
-        mHeaders?.append("\n#" + header.number + ": " + header.hash.hex.substring(0, 10) + "…")
+        headers?.append("\n#" + header.number + ": " + header.hash.hex.substring(0, 10) + "…")
     }
 
     override fun updatePeerInfos(peerInfos: PeerInfos) {
-        mPeers?.append(peerInfos.size().toString() + " ")
+        peers?.append(peerInfos.size().toString() + " ")
     }
 }
